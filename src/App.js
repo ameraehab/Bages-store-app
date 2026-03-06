1182c0b6181907bdedf87660d7903c308acc4f18
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-07 00:08:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
 import ProductsList from "./components/ProductsList";
+import SelectedCollection from "./components/SelectedCollection"
 import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Carousel />
-      <ProductsList />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Carousel />
+              <ProductsList />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/selected" element={<SelectedCollection />} />
+      </Routes>
+
     </div>
   );
 }
