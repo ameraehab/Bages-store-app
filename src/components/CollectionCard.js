597e5203ad72f0096fef482a192741930ef8464f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-07 00:10:23 +0200

    Update CollectionCard.js

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -3,30 +3,44 @@ import collection2 from "../assets/images/collection2.jpg";
 import collection3 from "../assets/images/collection3.jpg";
 import collection4 from "../assets/images/collection4.jpg";
 import collection5 from "../assets/images/collection5.jpg";
+import { Link } from "react-router-dom";
 
-const collectionsImages = [collection1, collection2, collection3, collection4, collection5];
+export const collections = [
+    { id: 1, name: "Classic Elegance", image: collection1 },
+    { id: 2, name: "Evening Glam", image: collection2 },
+    { id: 3, name: "Urban Chic", image: collection3 },
+    { id: 4, name: "Travel Buddy", image: collection4 },
+    { id: 5, name: "Jeune Premier", image: collection5 },
+];
+
 function CollectionCard({ showAll }) {
 
-    const visibleCollection = showAll ? collectionsImages : collectionsImages.slice(0, 4);
+    const visibleCollection = showAll ? collections : collections.slice(0, 4);
+
     return (
+
         <div className="container mx-auto p-5">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 justify-items-center">
+            <Link to="/selected">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 justify-items-center">
 
-                {
-                    visibleCollection.map((collection, index) => (
+                    {
+                        visibleCollection.map((collection, index) => (
+                            <img
+                                src={collection.image}
+                                key={collections.id}
+                                alt={`collection ${index}`}
+                                className="card w-full max-w-[255px] h-[328px] rounded-lg shadow-md cursor-pointer "
+                            />
 
-                        <img
-                            src={collection}
-                            key={index}
-                            alt={`collection ${index}`}
-                            className="w-full max-w-[255px] h-[330px] rounded-lg shadow-md"
-                        />
+                        ))
+                    }
 
-                    ))
+                </div>
+            </Link>
 
-                }
-            </div>
+
         </div>
+
     );
 }
 
