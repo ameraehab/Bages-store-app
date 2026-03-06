13f5ef38c19a89026f3437182c963c184da867fe
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-07 00:11:00 +0200

    Create SelectedCollection.js

---
+++ src/components/SelectedCollection.js
@@ -0,0 +1,7 @@
+function SelectedCollection() {
+    return (
+        <h1 className="text-xl sm:text-2xl font-bold text-orange-950 mb-2 sm:mb-0"> </h1>
+
+    )
+
+} export default SelectedCollection;
