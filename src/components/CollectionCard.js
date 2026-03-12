9eae70f29996ca40f600b6a01192f930e6ea06a8
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-12 04:51:09 +0200

    Update CollectionCard.js

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -5,41 +5,41 @@ import collection4 from "../assets/images/collection4.jpg";
 import collection5 from "../assets/images/collection5.jpg";
 import { Link } from "react-router-dom";
 
-export const collections = [
-    { id: 1, name: "Classic Elegance", image: collection1 },
-    { id: 2, name: "Evening Glam", image: collection2 },
-    { id: 3, name: "Urban Chic", image: collection3 },
-    { id: 4, name: "Travel Buddy", image: collection4 },
-    { id: 5, name: "Jeune Premier", image: collection5 },
-];
+
 
 function CollectionCard({ showAll }) {
-
+    const collections = [
+        { id: 1, name: "Classic Elegance", image: collection1 },
+        { id: 2, name: "Evening Glam", image: collection2 },
+        { id: 3, name: "Urban Chic", image: collection3 },
+        { id: 4, name: "Travel Buddy", image: collection4 },
+        { id: 5, name: "Jeune Premier", image: collection5 },
+    ];
     const visibleCollection = showAll ? collections : collections.slice(0, 4);
 
     return (
 
         <div className="container mx-auto p-5">
-            <Link to="/selected">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 justify-items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 justify-items-center">
 
-                    {
-                        visibleCollection.map((collection, index) => (
+                {
+                    visibleCollection.map((collection, index) => (
+                        <Link key={collection.id} to={`/collection/${collection.name}/${collection.id}`} >
                             <img
                                 src={collection.image}
-                                key={collections.id}
+                                key={collection.id}
                                 alt={`collection ${index}`}
-                                className="card w-full max-w-[255px] h-[328px] rounded-lg shadow-md cursor-pointer "
+                                className="card w-[280px] h-[328px] rounded-lg shadow-md cursor-pointer "
                             />
+                        </Link>
 
-                        ))
-                    }
+                    ))
+                }
 
-                </div>
-            </Link>
+            </div>
 
 
-        </div>
+        </div >
 
     );
 }
