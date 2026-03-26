5e2a5376c946304712df94e95d3b4252560bad3f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-27 00:58:10 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
-import { FaCar, FaCartPlus, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import CartList from "./CartList";
+import { useContext } from "react";
+import { CartContext } from "../Context/CartContext";
+
 function Navbar() {
+    const { bage } = useContext(CartContext);
+
     return (
-        <>
-            <nav className="border-b-2 p-4 flex justify-between items-center border-black ">
-                <ul className="flex gap-20">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <li>News</li>
-                </ul>
-                <a>mera</a>
-                <div className="flex items-center gap-3 ">
-                    <CartList />
-                    <div className="relative w-64">
-                        <FaSearch className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400" />
-                        <input
-                            type="search"
-                            placeholder="Search"
-                            className="w-full rounded-xl border-2 p-2 pl-7 cursor-pointer outline-none"
-                        />
+        <nav className="border-b-2 p-4 flex justify-between items-center border-black">
+            <ul className="flex gap-20">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <li>News</li>
+            </ul>
 
-                    </div>
+            <a>mera</a>
 
+            <div className="flex items-center gap-3">
+                <div className="relative">
+                    <Link to="/cart"> <FaShoppingCart className="text-xl cursor-pointer hover:text-orange-700" />
+
+                        <span className="absolute -top-2 -right-2 bg-orange-700 text-white text-xs px-1 rounded-full">
+                            {bage.length}
+                        </span></Link>
 
 
                 </div>
 
-            </nav>
 
-        </>
-
+                <div className="relative w-64">
+                    <FaSearch className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400" />
+                    <input
+                        type="search"
+                        placeholder="Search"
+                        className="w-full rounded-xl border-2 p-2 pl-7 outline-none"
+                    />
+                </div>
+            </div>
+        </nav>
     );
-
-
 }
+
 export default Navbar;
