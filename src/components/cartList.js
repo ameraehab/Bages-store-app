cbfba1b852dc8e2872c104df8a87f5f4288b8742
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-26 22:24:40 +0200

    Update cartList.js

---
+++ src/components/cartList.js
@@ -0,0 +1,14 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+function CartList(details) {
+    return (
+        <div>
+            <FaShoppingCart className="text-xl hover:cursor-pointer hover:text-orange-700 transition-colors" />
+
+        </div>
+
+    )
+
+}
+
+export default CartList;
