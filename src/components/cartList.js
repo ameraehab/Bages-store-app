1e251b4bb505f99db3dd890ddc5a54b483915c5f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-28 16:40:15 +0200

    Update cartList.js

--- src/components/cartList.js
+++ src/components/cartList.js
@@ -1,21 +1,38 @@
 import { useContext } from "react";
 import { CartContext } from "../Context/CartContext";
+import { MdRemoveShoppingCart } from "react-icons/md";
 
 function CartList() {
+    const { removeFromCart } = useContext(CartContext);
+
     const { bage } = useContext(CartContext);
     return (
-        <div className="p-5">
-            {bage.length === 0 ? (
-                <p> Cart is Empty 🛒</p>
-            ) : (
-                bage.map((item) => (
-                    <div key={item.id} className="border p-2 mb-2">
-                        <h3>{item.title}</h3>
-                        <p>{item.price}$</p>
-                    </div>
-                ))
-            )}
-        </div>
+        <>
+
+            <div className="p-5">
+                {bage.length === 0 ? (
+                    <p> Cart is Empty 🛒</p>
+                ) : (
+                    bage.map((item) => (
+                        <div>
+                            <div key={item.id} className="border p-2 mb-2">
+                                <img src={item.img} />
+                                <h3>{item.title}</h3>
+                                <p>{item.price}$</p>
+                            </div>
+                            <button onClick={() => removeFromCart()}>
+                                <MdRemoveShoppingCart />
+
+                            </button>
+
+                        </div>
+
+
+                    ))
+                )}
+            </div>
+        </>
+
     );
 }
 
