0190e50ab14b7a8b237f6338c3546c4a4cbb3f5c
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-28 16:40:35 +0200

    Update CartContext.js

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -3,6 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const CartContext = createContext();
 
 function CartProvider({ children }) {
+    const [showAlert, setAlert] = useState(false);
     const [bage, setBag] = useState(() => {
         const savedCart = localStorage.getItem("cart");
         return savedCart ? JSON.parse(savedCart) : [];
@@ -11,20 +12,44 @@ function CartProvider({ children }) {
         localStorage.setItem("cart", JSON.stringify(bage));
     }, [bage]);
 
+    const removeFromCart = (bage) => {
+        bage.remove(bage.id);
+    }
     const addToCart = (item) => {
         const exists = bage.some((bag) => bag.id === item.id);
 
         if (exists) {
-            console.log(`⚠️ ${item.title} موجود بالفعل في السلة!`, "warning");
+            setAlert(true);
+            setTimeout(() => {
+                setAlert(false);
+            }, 2000);
+
             return;
+
         }
 
         setBag((prev) => [...prev, item]);
     };
 
     return (
-        <CartContext.Provider value={{ bage, addToCart }}>
-            {children}
+
+        <CartContext.Provider value={{ bage, addToCart, removeFromCart }}>
+            <>
+                {showAlert && (
+                    <div className="fixed top-20 right-5 z-50">
+                        <div
+                            role="alert"
+                            className="bg-orange-700 text-white px-4 py-2 rounded-lg shadow-lg flex items-center animate-slideIn"
+                        >
+                            <p className="text-sm font-semibold">
+                                The bag is already in the shopping cart 🛒
+                            </p>
+                        </div>
+                    </div>
+                )}
+                {children}
+
+            </>
         </CartContext.Provider>
     );
 }
