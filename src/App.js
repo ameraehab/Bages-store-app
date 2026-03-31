457ae4bc48a3a6016339b95f29661ce45e313626
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-03-31 17:50:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import CartProvider from "./Context/CartContext";
 import CartList from "./components/CartList";
+import Footer from "./components/Footer";
 function App() {
 
   return (
     <CartProvider>
-      <div className="App">
-
+      <div className="App flex flex-col min-h-screen">
         <Navbar />
 
+        <main className="flex-1">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Carousel />
+                  <ProductsList />
+                </>
+              }
+            />
+            <Route path="/about" element={<About />} />
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Carousel />
-                <ProductsList />
-              </>
-            }
-          />
-          <Route path="/about" element={<About />} />
+            <Route path="/collection/:collectionName/:collectionNumber" element={<SelectedCollection />} />
 
-          <Route path="/collection/:collectionName/:collectionNumber" element={<SelectedCollection />} />
+            <Route path="/cart" element={<CartList />} />
 
-          <Route path="/cart" element={<CartList />} />
+          </Routes>
 
-        </Routes>
-
+        </main>
+        <Footer />
       </div>
     </CartProvider>
 
