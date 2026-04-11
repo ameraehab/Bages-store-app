0bb3e4dfbab597d2878b172d846228aa33fce0ef
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-04-11 22:10:55 +0200

    Update SelectedCollection.js

--- src/components/SelectedCollection.js
+++ src/components/SelectedCollection.js
@@ -83,18 +83,7 @@ function SelectedCollection() {
     const currentBags = collectionBags.slice(startIndex, startIndex + itemsPerPage);
     const totalPages = Math.ceil(collectionBags.length / itemsPerPage);
 
-    if (loading) {
-        return (
-            <div className="p-5 min-h-[400px] flex items-center justify-center h-screen">
-                <div className="loadingio-spinner-spinner-977el9wwy2v">
-                    <div className="ldio-4j5ay0xf86g">
-                        <div></div><div></div><div></div><div></div><div></div><div></div>
-                        <div></div><div></div><div></div><div></div><div></div><div></div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+
 
     if (collectionBags.length === 0) {
         return (
@@ -105,61 +94,75 @@ function SelectedCollection() {
     }
 
     return (
-        <div className="p-5">
-            <h1 className="text-xl sm:text-2xl font-bold text-orange-950 mb-4">
-                {collectionName}
-            </h1>
+        <>
+            {
+                loading ? (<div className="p-5 min-h-[400px] flex items-center justify-center h-screen" >
+                    <div className="loadingio-spinner-spinner-977el9wwy2v">
+                        <div className="ldio-4j5ay0xf86g">
+                            <div></div><div></div><div></div><div></div><div></div><div></div>
+                            <div></div><div></div><div></div><div></div><div></div><div></div>
+                        </div>
+                    </div>
+                </div>) : (
+                    <div className="p-5">
+                        <h1 className="text-xl sm:text-2xl font-bold text-orange-950 mb-4">
+                            {collectionName}
+                        </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-                {currentBags.map((bag) => (
-                    <div
-                        key={bag.id}
-                        className="shadow rounded p-3 hover:shadow-lg hover:scale-105 transition-transform duration-300 relative"
-                    >
-                        <ProductImage
-                            src={bag.image}
-                            alt={bag.title}
-                            className="w-80 h-52 object-center mb-2 rounded"
-                        />
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                            {currentBags.map((bag) => (
+                                <div
+                                    key={bag.id}
+                                    className="shadow rounded p-3 hover:shadow-lg hover:scale-105 transition-transform duration-300 relative"
+                                >
+                                    <ProductImage
+                                        src={bag.image}
+                                        alt={bag.title}
+                                        className="w-80 h-52 object-center mb-2 rounded"
+                                    />
 
-                        <h3 className="font-semibold mt-2">{bag.title}</h3>
-                        <p className="text-orange-700 font-bold">{bag.price}$</p>
-                        {user ? (
-                            <button
-                                className="absolute top-4 right-4 bg-orange-600 text-white p-2 rounded-full hover:bg-orange-700 transition-colors z-20"
-                                title="Add to Cart"
-                                onClick={() => addToCart(bag, bag.Quantity, bag.price)}
-                            >
-                                <FaCartPlus size={20} />
-                            </button>
-                        ) : (
-                            <div className="absolute top-4 right-4 text-xs bg-orange-600 text-white p-2 rounded p-1">
-                                Login to add
+                                    <h3 className="font-semibold mt-2">{bag.title}</h3>
+                                    <p className="text-orange-700 font-bold">{bag.price}$</p>
+                                    {user ? (
+                                        <button
+                                            className="absolute top-4 right-4 bg-orange-600 text-white p-2 rounded-full hover:bg-orange-700 transition-colors z-20"
+                                            title="Add to Cart"
+                                            onClick={() => addToCart(bag, bag.Quantity, bag.price)}
+                                        >
+                                            <FaCartPlus size={20} />
+                                        </button>
+                                    ) : (
+                                        <div className="absolute top-4 right-4 text-xs bg-orange-600 text-white p-2 rounded p-1">
+                                            Login to add
+                                        </div>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+
+                        {totalPages > 1 && (
+                            <div className="flex justify-center mt-6 gap-2">
+                                {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                                    (page) => (
+                                        <button
+                                            key={page}
+                                            onClick={() => setCurrentPage(page)}
+                                            className={`px-3 py-1 rounded ${page === currentPage
+                                                ? "bg-orange-600 text-white"
+                                                : "bg-gray-200"
+                                                }`}
+                                        >
+                                            {page}
+                                        </button>
+                                    )
+                                )}
                             </div>
                         )}
                     </div>
-                ))}
-            </div>
+                )
+            }
+        </>
 
-            {totalPages > 1 && (
-                <div className="flex justify-center mt-6 gap-2">
-                    {Array.from({ length: totalPages }, (_, i) => i + 1).map(
-                        (page) => (
-                            <button
-                                key={page}
-                                onClick={() => setCurrentPage(page)}
-                                className={`px-3 py-1 rounded ${page === currentPage
-                                    ? "bg-orange-600 text-white"
-                                    : "bg-gray-200"
-                                    }`}
-                            >
-                                {page}
-                            </button>
-                        )
-                    )}
-                </div>
-            )}
-        </div>
     );
 }
 
